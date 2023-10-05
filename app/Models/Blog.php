9b933b35375ea5ded105af33d11a9128e73d143c
KAILASHGANGAI <47468706+KAILASHGANGAI@users.noreply.github.com>
2023-10-05 10:24:49 +0545

    initial setup of model and migration

---
+++ app/Models/Blog.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Blog extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'image'
+    ];
+
+    public function user():HasOne
+    {
+       return $this->hasOne(User::class);
+    }
+    
+}
